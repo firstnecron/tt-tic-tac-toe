5578c7667feb0114ba6a563456c6ce938fdf9310
Justin Russo <firstnecron@gmail.com>
2017-02-06 03:20:05 -0500

    Player & UI Add playerNum property Add setActivePlayer function Use the proper variable for player1.mark in showPlayer2Prompt

--- js/player.js
+++ js/player.js
@@ -1,6 +1,7 @@
 !function($, exports) {
     // Player
     var Player = function (playerName, markType, playerType) {
+        this.playerNum = 0;
         this.playerName = playerName;
         this.mark = markType;
         this.playerType = playerType;
