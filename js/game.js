2722d820a5a6f2466122166f64b6183d287b1710
Justin Russo <firstnecron@gmail.com>
2017-02-06 01:24:31 -0500

    Game Add players to Session

--- js/game.js
+++ js/game.js
@@ -1,13 +1,15 @@
 !function($, exports) {
     // Game
     // Session (play again continues, new game resets)
-    var Session = function () {
+    var Session = function (player1, player2) {
         this.gamesPlayed = 0;
         this.movesMade = 0;
         this.score = {
             player1: 0,
             player2: 0
         };
+        this.player1 = player1;
+        this.player2 = player2;
     };
 
     exports.Session = Session;
